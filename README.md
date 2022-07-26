7b1d02935bf87e07f65ae102217f0a9502652a56
Michael Andrews <michael.andrews@pega.com>
2022-07-26 18:35:34 -0400

    Refactored some of the code

--- README.md
+++ README.md
@@ -1,28 +1,33 @@
+# Professional Readme generator
 
-# Professional-README-Generator
-https://github.com/mrandrews78/Professional-README-Generator
+https://github.com/mrandrews78/Professional Readme generator
 
-## Description 
-This program will create a readme file after collecting input from the users responses to questions.
+undefined
+
+## Description
+
+The applcation allows you to create a professional readme generator after answering a series of questions.
+
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
 - [Tests](#tests)
 - [Credits](#credits)
 - [License](#license)
+
 ## Installation
 The following dependencies must be installed to run the application
-You will need to install nodejs and use the inquirer package
+You will need to install nodejs and inquirer package
+
 ## Usage
-In order to use this application, type the command node index.js in terminal
+In order to use this application, Type the command node index.js
+
 ## Tests
-There were no tests written for this application.
+no test were given
+
 ## Credits
-There were no collaborators.
-## License
+there are no collaborators at this time
 
-This project is licensed under the no license license. Please click here for more info: 
-
-# Contact
-If you have any questions about the repository or about this application please send an email to: mrandrews78@gmail.com
+## Contact
+If you have any questions about the repository or about this application please send an email to: mandrews@gmail.com
 - Github: mrandrews78
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // External Packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // Array of questions for the user.
 const questions = [
@@ -10,84 +10,88 @@ const questions = [
     type: 'input',
     message: 'What is the name or title of your project? ',
     name: 'title',
-    //validate property to check that the user entered a name or title.
-    validate: (answer) => {if (answer) {return true} else {return 'Please enter a title, to continue'}}
+    // validate property to check that the user entered a name or title.
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please enter a title, to continue'}}
   },
   {
     type: 'input',
     message: 'Please give a brief description of your project: ',
     name: 'description',
-    validate: (answer) => {if (answer) {return true} else {return 'Please enter a title, to continue'}}
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please enter a title, to continue'}}
   },
   {
     type: 'input',
     message: 'Pleasa provide install dependencies for your application for the Installation section?',
     name: 'installation',
-    validate: (answer) => {if (answer) {return true} else {return 'Please provide installation steps, to continue'}}
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please provide installation steps, to continue'}}
   },
   {
     type: 'input',
     message: 'How do we use your application? ',
     name: 'usage',
-    validate: (answer) => {if (answer) {return true} else {return 'Please provide usage instructions to continue'}}
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please provide usage instructions to continue'}}
   },
   {
     type: 'input',
     message: 'Please provide any tests written for your application and how to run them.',
     name: 'tests',
-    validate: (answer) => {if (answer) {return true} else {return 'Please provide tests if any to continue'}}
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please provide tests if any to continue'}}
   },
   {
     type: 'input',
     message: 'List your collaborators, if any, with links to their GitHub profiles.',
     name: 'credits'
-    
+
   },
   {
-    //List of type of licenses to use
+    // List of type of licenses to use
     type: 'list',
     message: 'Choose a license for your project? ',
-    choices: ['AFL 3.0', 'GPL', 'MIT', 'MPL-2.0', 'no license'],
+    choices: ['AFL 3.0', 'GPL', 'MIT', 'MPL-2.0'],
     name: 'license'
   },
   {
     type: 'input',
     message: 'What is your GitHub username? ',
     name: 'username',
-    validate: (answer) => {if (answer) {return true} else {return 'Please provide a GitHub username to continue'}}
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please provide a GitHub username to continue'}}
   },
   {
     type: 'input',
     message: 'What is your email address?',
     name: 'email',
-    validate: (answer) => {if (answer) {return true} else {return 'Please enter an email address, to continue'}}
-  },
-];
+    validate: (answer) => {
+      if (answer) {return true} else {return 'Please enter an email address, to continue'}}
+  }
+]
 
+// Function to create README file using fs package
+function createReadMe (fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    console.log(fileName)
+    console.log(data)
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log('success')
+    }
+  })
+}
 
-//Function to create README file using fs package
-function createReadMe(fileName,data){
-    fs.writeFile(fileName, data, function(err) {
-        console.log(fileName)
-        console.log(data)
-        if (err) {
-            return console.log(err)
-        } else {
-            console.log("success")
-        }
+// function to iniitialize the application
+function init () {
+  inquirer.prompt(questions)
+    .then(function (data) {
+      createReadMe('README.md', generateMarkdown(data))
+      console.log(data)
     })
 }
 
-//function to iniitialize the application
-function init(){
-    inquirer.prompt(questions)
-    .then(function(data){
-        createReadMe("README.md", generateMarkdown(data));
-        console.log(data)
-    })
-}
-
-
-//function call to intialize the application
-init();
-        
+// function call to intialize the application
+init()
